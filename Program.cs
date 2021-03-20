dd10f1415ba214a7016300f845960eb9ef2fb2f1
Diego Neri <diego@diegoneri.com>
2021-03-19 22:45:33 -0300

    Corrige as substituições de acordo com o exercício
    Haviam alguns replaces repetidos. Foi corrigido de acordo com o exercício https://github.com/ermogenes/aulas-programacao-csharp/blob/master/exercises/entrada-strings.md#exerc%C3%ADcio-leet

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace Leet
             Console.WriteLine("Hackers usavam leet antes de virar modinha");
             Console.WriteLine("Texto a ser traduzido");
             frase = Console.ReadLine();
-            leet = frase.Replace("a", "4").Replace("e", "3").Replace("s", "5").Replace("s", "5").Replace("a", "4").Replace("a", "4").Replace("i", "1").Replace("e", "3").Replace("e", "3").Replace("t", "7").Replace("a", "4").Replace("t", "7").Replace("e", "3").Replace("s", "5").Replace("e", "3").Replace("I", "1").Replace("a", "4").Replace("o", "0").Replace("i", "1").Replace("a", "4");
+            leet = frase.Replace("a", "4").Replace("A", "4").Replace("e", "3").Replace("E", "3").Replace("i", "1").Replace("I", "1").Replace("l", "1").Replace("L", "1").Replace("O", "0").Replace("o", "0").Replace("t", "7").Replace("T", "7").Replace("s", "5").Replace("S", "5");
+
             Console.WriteLine($"Texto leet: {leet}"); 
 
 
